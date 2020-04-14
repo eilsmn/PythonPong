c836e3c781be2eecc073835dd8e6fc572ff61846
Simon Eiler <eilersimon@web.de>
2020-04-14 23:04:43 +0200

    add score for player a and player b

--- pong.py
+++ pong.py
@@ -10,10 +10,13 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(0) #animation speed, not movement speed
 paddle_a.shape("square")
 paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
@@ -39,6 +42,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.125
 ball.dy = -0.125
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Function
 def paddle_a_up():
@@ -86,14 +99,20 @@ while True:
         ball.sety(-290)
         ball.dy *= -1
 
-    # Border checking left and right
+    # Border checking right and left
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if (ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50) and (ball.ycor() > paddle_b.ycor() - 50):
